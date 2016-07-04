0f8ade34b986c23b41e4c649801424eb64f21789
Jesus Alberto Ramirez Popo <kcho.ramirezp@gmail.com>
2016-07-04 12:40:40 -0500

    fix injection controller into web page

--- restFullSep/restFullSep/app.js
+++ restFullSep/restFullSep/app.js
@@ -1,11 +1,18 @@
 ﻿(function () {
     'use strict';
-    angular.module('app', ['ngRoute']).controller(function ($scope) {
+    angular.module('vsAngular', []).controller("Controlador1", function ($scope, $log) {
 
         $scope.listaProyectos = [
            { nombre: "A", lenguaje: "Visual C#", tipo: "Web", horas: 1000 },
            { nombre: "B", lenguaje: "Visual Basic", tipo: "Mobile", horas: 250 },
            { nombre: "C", lenguaje: "TypeScript", tipo: "Web", horas: 2000 }];
+
+        $scope.verDetalle = function (proyecto) {
+            
+            $scope.proyectoDetalle = proyecto;
+            $log.log(proyecto);
+            
+        }
 
     })
 })
